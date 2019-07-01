24bfb6daaeae7c95f2a1a487305de747514436c5
aloosh123 <42175145+aloosh123@users.noreply.github.com>
2019-07-01 04:59:07 +0300

    Add files via upload

---
+++ 00ali.js
@@ -0,0 +1,8 @@
+$('button#login-form__submit').mouseenter(function()
+{
+$(this).css("color","cornflowerblue");
+});
+$('button#login-form__submit').mouseleave(function()
+{
+$(this).css("color","#f8f9fa");
+});
